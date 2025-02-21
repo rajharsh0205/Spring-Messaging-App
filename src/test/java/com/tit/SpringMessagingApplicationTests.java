b34bc6ef21d71eddb4d9f90f88888bafaa89c734
rajharsh0205 <rajharsh810201@gmail.com>
2025-02-21 23:16:32 +0530

    [Harsh] Add . Merged all features

---
+++ src/test/java/com/tit/SpringMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
